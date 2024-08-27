6d67ec3f241e453f5d3c839a189ccd059830f644
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2024-08-27 15:24:04 -0700

    submitted?

--- src/routes/[[preview=preview]]/contact/+page.svelte
+++ src/routes/[[preview=preview]]/contact/+page.svelte
@@ -45,7 +45,7 @@
         <h6 class="md:w-1/5 text-primary my-4">Via Email</h6>
         <div class="w-full md:w-4/5 flex flex-col gap-8">
             <h5>Complete this form and we'll get back to you.</h5>
-            <form class="w-full h-full flex flex-col md:flex-row px-8"  name="contact" method="post" data-netlify="true"  data-netlify-honeypot="bot-field">
+            <form class="w-full h-full flex flex-col md:flex-row px-8"  name="contact" method="POST" action="/contact?submitted" data-netlify="true"  data-netlify-honeypot="bot-field">
                 <Turnstile siteKey="0x4AAAAAAAh2fGW6xIcdsqNr" />
                 <input type="hidden" name="form-name" value="contact" />
                     <div class="h-full w-full mt-8 md:mt-0 md:w-2/3 flex flex-col gap-4 items-start">
