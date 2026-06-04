87f1b3434d7b4dfc96808696bbc03889eeb78733
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2026-06-04 11:50:26 -0700

    chore: sync svelte config from @reddoorla/maintenance

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,7 @@
+import { createSvelteConfig } from "@reddoorla/maintenance/configs/svelte";
 import adapter from "@sveltejs/adapter-netlify";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-
-const silencedWarnings = new Set(["element_invalid_self_closing_tag"]);
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-  kit: {
-    adapter: adapter(),
-    prerender: {
-      handleHttpError: "warn",
-    },
-  },
-  preprocess: vitePreprocess(),
-  compilerOptions: {
-    warningFilter: (warning) => !silencedWarnings.has(warning.code),
-  },
-};
-
-export default config;
+export default createSvelteConfig({
+  kit: { adapter: adapter({ edge: false, split: false }) },
+});
